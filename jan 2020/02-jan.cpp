c80d7543adc60fa71a8e4195142e1de3ca96cedc
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-02 18:59:08 +0200

    ADD jan 2nd challenge solution

---
+++ jan 2020/02-jan.cpp
@@ -0,0 +1,31 @@
+//
+// Created by ahmeddrawy on 1/2/2021.
+//
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(cloned->val == target->val){
+            return cloned;
+        }
+        if(cloned->right == NULL && cloned->left== NULL)
+            return NULL;
+        if(cloned->left != NULL){
+            TreeNode * l = cloned->left;
+            TreeNode * ret = getTargetCopy(original , l , target);
+            if(cloned->right != NULL){
+                TreeNode * r = cloned->right;
+                if(ret == NULL)
+                    return getTargetCopy(original , r , target);
+
+            }
+
+            return ret;
+        }
+        else {
+            TreeNode * r = cloned->right;
+            return getTargetCopy(original , r , target);
+        }
+    }
+
+};
